c733b0cb3a64e8e03aae455999e9000b2a246af9
Rafaek Galuh <galprad99@gmail.com>
2022-02-18 05:09:09 +0700

    dd publish

--- app/Http/Controllers/PublishController.php
+++ app/Http/Controllers/PublishController.php
@@ -145,10 +145,10 @@ public function apimassunpublish(Request $request){
         $arr = [];
         foreach($request->product_id as $key => $pid){
             $product = Product::where('product_id',$pid)->first();
-            $product->product_tag = $request->product_tag[$key]??null;
-            $product->product_material = $request->product_material[$key]??null;
-            $product->product_madein = $request->product_madein[$key]??null;
-            $product->product_condition = $request->product_condition[$key]??null;
+            $product->product_tag = $request->product_tag[$key];
+            $product->product_material = $request->product_material[$key];
+            $product->product_madein = $request->product_madein[$key];
+            $product->product_condition = $request->product_condition[$key];
             $product->product_tanggalpublish = $request->publish_tanggal;
             if($product->product_status == 1){
                 $product->product_stok = $request->product_stok[$key];
@@ -175,7 +175,7 @@ public function apimassunpublish(Request $request){
         }
          toast('Ubah Publish Berhasil','success');
         // return redirect('publish');
-        return $request->product_id;
+        return dd($request->product_id);
     }
 
     public function delete($groupid)
